3a8a8cbde310e5683980f26e1e90b1f9265bf4e2
Andres Galvan <afgalvan@unicesar.edu.co>
2021-07-06 00:12:57 -0500

    Generate sequences

--- Linq/Linq.Test/Generation/GenerateSequences.cs
+++ Linq/Linq.Test/Generation/GenerateSequences.cs
@@ -1,9 +1,25 @@
 using NUnit.Framework;
+using System;
+using System.Linq;
+using EnumerableExtensions;
 
 namespace Linq.Test.Generation
 {
     [TestFixture]
     public class GenerateSequences
     {
+        [Test]
+        public void CreateRangeOfNumbers() =>
+            Enumerable.Range(100, 50)
+                .Select(n => (Number: n, OddEven: n % 2 == 0 ? "even" : "odd"))
+                .ForEach(n => Console.WriteLine("The number {0} is {1}.",
+                    n.Number, n.OddEven));
+
+        [Test]
+        public void RepeatNumber()
+        {
+            var numbers = Enumerable.Repeat(7, 10);
+            numbers.ForEach(Console.WriteLine);
+        }
     }
 }
